2a952d338c34452e19769a4780f388948fea39c0
Tiago Dias <tiago.dias@simbiose.com>
2018-05-07 11:21:32 +0100

    style: restaurant reviews

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -119,18 +119,22 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.className = 'reviews-name';
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.className = 'reviews-date';
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.className = 'reviews-rating';
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.className = 'reviews-comments';
   li.appendChild(comments);
 
   return li;
