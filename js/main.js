c50dda4966b366abc5d6195b334d092d6277580a
Tiago Dias <tiago.dias@simbiose.com>
2018-06-10 20:00:37 +0100

    fix: project review fixes some a11y fixes based on project reviewer suggestions

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ createRestaurantHTML = (restaurant) => {
   image.title = DBHelper.imageAltForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
