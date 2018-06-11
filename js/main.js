4ac4e9a6468e1393bdc25f9c693e8a85597995d6
Tiago Dias <tiago.dias@simbiose.com>
2018-06-11 17:00:17 +0100

    feat: leaflet instead of google maps

--- js/main.js
+++ js/main.js
@@ -10,13 +10,14 @@ if ('serviceWorker' in navigator) {
 let restaurants,
   neighborhoods,
   cuisines
-var map
+var newMap
 var markers = []
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -78,20 +79,37 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 }
 
 /**
- * Initialize Google map, called from HTML.
+ * Initialize leaflet map, called from HTML.
  */
-window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  updateRestaurants();
-}
+ initMap = () => {
+   self.newMap = L.map('map', {
+         center: [40.722216, -73.987501],
+         zoom: 12,
+         scrollWheelZoom: false
+       });
+   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+     mapboxToken: 'pk.eyJ1IjoidC1tYWQiLCJhIjoiY2ppYWUycGx4MTNmYTN3bWxlMXFkZ2dpNiJ9.Ts9mBSf986SO2Y8hZCREqQ',
+     maxZoom: 18,
+     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+     id: 'mapbox.streets'
+   }).addTo(newMap);
+
+   updateRestaurants();
+ }
+// window.initMap = () => {
+//   let loc = {
+//     lat: 40.722216,
+//     lng: -73.987501
+//   };
+//   self.map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 12,
+//     center: loc,
+//     scrollwheel: false
+//   });
+//   updateRestaurants();
+// }
 
 /**
  * Update page and map for current restaurants.
@@ -183,13 +201,25 @@ createRestaurantHTML = (restaurant) => {
 /**
  * Add markers for current restaurants to the map.
  */
-addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-}
+
+ addMarkersToMap = (restaurants = self.restaurants) => {
+   restaurants.forEach(restaurant => {
+     // Add marker to the map
+     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+     marker.on("click", onClick);
+     function onClick() {
+       window.location.href = marker.options.url;
+     }
+   });
+ }
+
+// addMarkersToMap = (restaurants = self.restaurants) => {
+//   restaurants.forEach(restaurant => {
+//     // Add marker to the map
+//     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
+//     google.maps.event.addListener(marker, 'click', () => {
+//       window.location.href = marker.url
+//     });
+//     self.markers.push(marker);
+//   });
+// }
